d69c457c8e2e224a915289c36351e5b76a72285e
Lydia Rakel Cortez García <6051135@incb.edu.sv>
2026-05-27 07:56:42 -0600

    Documentadas consultas avanzadas y reportes con INNER JOIN

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,36 +1,39 @@
-CREATE DATABASE sistema_inventario;
-
-
 USE sistema_inventario;
 
-
+-- 1. Tabla para el módulo de Login y Seguridad
 CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_completo VARCHAR(100) NOT NULL,
-    usuario VARCHAR(50) NOT NULL UNIQUE, 
-    password VARCHAR(255) NOT NULL,      
-    rol VARCHAR(20) NOT NULL
+id INT AUTO_INCREMENT PRIMARY KEY,
+nombre_completo VARCHAR(100) NOT NULL,
+usuario VARCHAR(50) NOT NULL UNIQUE,
+password VARCHAR(255) NOT NULL,
+rol VARCHAR(20) NOT NULL
 );
 
+-- 2. NUEVA TABLA RAÍZ: Categorías del sistema
+CREATE TABLE categorias (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nombre_categoria VARCHAR(50) NOT NULL UNIQUE);
 
 CREATE TABLE productos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_producto VARCHAR(100) NOT NULL,
-    categoria VARCHAR(50) NOT NULL,
-    stock INT NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL      
+id INT AUTO_INCREMENT PRIMARY KEY,
+nombre_producto VARCHAR(100) NOT NULL,
+categoria_id INT NOT NULL,
+stock INT NOT NULL,
+precio DECIMAL(10, 2) NOT NULL,
+FOREIGN KEY (categoria_id) REFERENCES categorias(id)
 );
 
-INSERT INTO usuarios (nombre_completo, usuario, password, rol) VALUES
-('Administrador Principal', 'admin', 'admin123', 'Administrador'),
-('Cajero de Turno', 'cajero1', 'ventas2024', 'Cajero');
+-- 4. Inserción de los catálogos base (deben insertarse primero)
+INSERT INTO categorias (nombre_categoria) VALUES
+('Computadoras'),
+('Accesorios'),
+('Oficina');
 
-
-INSERT INTO productos (nombre_producto, categoria, stock, precio) VALUES
-('Laptop Dell Inspiron 15', 'Computadoras', 10, 650.00),
-('Mouse Inalámbrico Logitech', 'Accesorios', 25, 15.50),
-('Impresora Epson EcoTank', 'Oficina', 5, 210.00),
-('Resma de Papel Tamaño Carta', 'Papelería', 100, 4.25);
+-- 5. Inserción de Productos (Observa cómo ahora vinculamos usando números
+enteros)
+INSERT INTO productos (nombre_producto, categoria_id, stock, precio) VALUES
+('Laptop Dell Inspiron 15', 1, 15, 720.00),
+('Mouse Inalámbrico Logitech', 2, 25, 12.00);
 
 SELECT * FROM productos WHERE id = 2;
 
@@ -38,3 +41,7 @@ UPDATE productos SET precio = 12.00 WHERE id = 2;
 
 DELETE FROM productos WHERE id = 3;
 
+SELECT p.id, p.nombre_producto, c.nombre_categoria, p.stock, p.precio FROM productoss p INNER JOIN categorias c ON p.categoria_id = c.id;
+
+SELECT p.id, p.nombre_producto, c.nombre_categoria, p.stock, p.precio FROM productoss p
+INNER JOIN categorias c ON p.categoria_id = c.id WHERE c.nombre_categoria = 'Accesorios';
